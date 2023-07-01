4a3a589599b74f92cc26889213afab1723302426
Ivan Firestone <ivan.firestone@icloud.com>
2023-07-01 15:46:27 -0400

    Calculadora Clases 11 Marzo 2023

---
+++ Form1.cs
@@ -0,0 +1,100 @@
+namespace P2_2023_Calculadora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double value1;
+        double value2;
+        string operador;
+        double resultado;
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_suma_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_resta_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_multiplicar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_dividir_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_ejecutar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_limpiar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_borrar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
